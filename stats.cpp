2a27b545ceea4a64e53c1c0fc316c7ee7273cdba
Darvey <darvey6@hotmail.com>
2019-03-27 10:35:56 -0700

    stats function

--- stats.cpp
+++ stats.cpp
@@ -2,9 +2,67 @@
 #include "stats.h"
 
 stats::stats(PNG & im){
+  // initialize all private vectors so that, for each color channel,
+  // (x,y) is the cumulative sum of the the color values from (0,0)
+  // to (x,y), and the hist structure is as defined above.
+    // Note that the hue (h) value of each pixel is represented by
+    // its cartesian coordinates: X = s*cos(h) and Y = s*sin(h).
+    // This is done to simplify distance computation. We calculate
+    // the cumulative sums for X and Y separately, and then combine
+    // them when we are doing color difference computation.
 
 /* your code here */
 
+  hist = vector<vector<vector<int>>> (0, vector<vector<int>>(0, vector<int>(36, 0)));
+  sumHueX  = vector<vector<double>> (0, vector<double>(0));
+  sumHueY = vector<vector<double>> (0, vector<double>(0));
+  sumSat =  vector<vector<double>> (0, vector<double>(0));
+  sumLum  = vector<vector<double>> (0, vector<double>(0));
+
+  int totalX = 0;
+  int totalY = 0;
+  int totalSat = 0;
+  int totalLum = 0;
+
+  for(int j = 0; j<im.height(); j++){
+    totalX = 0;
+    totalY = 0;
+    totalSat = 0;
+    totalLum = 0;
+    for(int i = 0; i<im.width(); i++){
+      HSLAPixel *pix1 = im.getPixel(i,j);
+
+      sumHueX[j][i] = pix1->s * cos(pix1->h);
+      sumHueY[j][i] = pix1->s * sin(pix1->h);
+      sumSat[j][i] = pix1->s;
+      sumLum[j][i] = pix1->l;
+
+      totalX = totalX + sumHueX[j][i];
+      totalY = totalY + sumHueY[j][i];
+      totalSat = totalSat  + sumSat[j][i];
+      totalLum = totalLum + sumLum[j][i];
+
+      if (j > 0){
+        sumHueX[j][i] = totalX + sumHueX[j-1][i];
+        sumHueY[j][i] = totalY + sumHueY[j-1][i];
+        sumSat[j][i] = totalSat + sumSat[j-1][i];
+        sumLum[j][i] = totalLum + sumLum[j-1][i];
+      }
+      for(int k = 0; k < 36; k++){
+        if (i == 0 && j == 0){
+          break;
+        }
+        if (i == 0){
+          hist[i][j][k] = hist[i-1][j][k];
+        }
+        if (j == 0){
+          hist[i][j][k] = hist[i][j-1][k];
+        }
+        hist[i][j][k] = hist[i][j-1][k] + hist[i-1][j][k] - hist[i-1][j-1][k];
+      }
+        hist[i][j][pix1->h/10]++;
+    }
+  }
 }
 
 long stats::rectArea(pair<int,int> ul, pair<int,int> lr){
@@ -32,8 +90,8 @@ double stats::entropy(vector<int> & distn,int area){
 /* your code here */
 
     for (int i = 0; i < 36; i++) {
-        if (distn[i] > 0 ) 
-            entropy += ((double) distn[i]/(double) area) 
+        if (distn[i] > 0 )
+            entropy += ((double) distn[i]/(double) area)
                                     * log2((double) distn[i]/(double) area);
     }
 
